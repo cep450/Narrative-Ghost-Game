90195fd5c44a7b1f396b0768924e4f474c4e2bc5
jaden <cep450@nyu.edu>
2021-11-18 16:02:52 -0500

    player movement

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class Controls : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float MAX_POS_X = 20f;
+    float MAX_POS_Y = 20f;
+    static float SPEED = 0.05f;
+    float SPEED_DIAG = SPEED * Mathf.Sin(Mathf.PI / 4) + 0.002f;
+
+    Vector3 faceright = new Vector3(1, 1, 1);
+    Vector3 faceleft = new Vector3(-1, 1, 1);
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //only check key once
+        bool up = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
+        bool down = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
+        bool left = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+        bool right = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+
+        float effectivespeed;
+
+        //same speed when diagonal
+        if((up || down ) && (left || right)) {
+            effectivespeed = SPEED_DIAG;
+        } else {
+            effectivespeed = SPEED;
+        }
+
+        //prevent from going out of bounds
+        if(up && transform.position.y < MAX_POS_Y) {
+            transform.Translate(0f, effectivespeed, 0f);
+        }
+        if(down && transform.position.y > -MAX_POS_Y) {
+            transform.Translate(0f, -effectivespeed, 0f);
+        }
+        if(left && transform.position.x > -MAX_POS_X) {
+            transform.Translate(-effectivespeed, 0f, 0f);
+        }
+        if(right && transform.position.x < MAX_POS_X) {
+            transform.Translate(effectivespeed, 0f, 0f);
+        }
+
+        //flip sprite 
+        if(right) {
+            transform.localScale = faceright;
+        } else if(left) {
+            transform.localScale = faceleft;
+        }
+
     }
 }
