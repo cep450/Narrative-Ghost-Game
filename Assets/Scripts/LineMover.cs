37313fd739446314c23bd9ab738ac908b97cd985
jaden <cep450@nyu.edu>
2021-12-09 13:42:40 -0500

    dialogue moves forward!

---
+++ Assets/Scripts/LineMover.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Yarn.Unity {
+public class LineMover : MonoBehaviour
+{
+
+    public DialogueUI dialogueUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startWaitNextLine() {
+        StartCoroutine(waitNextLine());
+    }
+
+    public IEnumerator waitNextLine() {
+        yield return new WaitForSeconds(5);
+        dialogueUI.MarkLineComplete();
+
+    }
+}
+}
